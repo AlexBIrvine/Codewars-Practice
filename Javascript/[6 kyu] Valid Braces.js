391da53f22231e8542877f8082b100062fe7f18b
Alex Irvine <Alex.b.Irvine@gmail.com>
2020-01-15 07:59:48 -0500

    More practice

---
+++ Javascript/[6 kyu] Valid Braces.js
@@ -0,0 +1,35 @@
+// Write a function that takes a string of braces, and determines if the order of the braces is valid.
+//It should return true if the string is valid, and false if it's invalid.
+// All input strings will be nonempty, and will only consist of parentheses, brackets and curly braces: ()[]{}.
+
+// What is considered Valid?
+// A string of braces is considered valid if all braces are matched with the correct brace.
+
+// Examples
+// "(){}[]"   =>  True
+// "([{}])"   =>  True
+// "(}"       =>  False
+// "[(])"     =>  False
+// "[({})](]" =>  False
+
+function validBracesRefactored(braces) {
+  for (var i = 0, chars = []; i < braces.length; i++) {
+    switch (braces[i]) {
+      case '(':
+      case '[':
+      case '{':
+        chars.push(braces[i]);
+        break;
+      case ')':
+        if (chars.pop() != '(') return false;
+        break;
+      case ']':
+        if (chars.pop() != '[') return false;
+        break;
+      case '}':
+        if (chars.pop() != '{') return false;
+        break;
+    }
+  }
+  return chars.length == 0;
+}
